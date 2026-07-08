21d91bcb3c4c9934f54335582f3f92fee7649f46
hgkimcau <132544832+hgkimcau@users.noreply.github.com>
2026-07-08 16:36:16 +0900

    Update about.js

--- data/about.js
+++ data/about.js
@@ -1,6 +1,6 @@
 export const ABOUT = {
   bio: [
-    "I am an Associate Professor in the [Graduate School of Advanced Imaging Science, Multimedia & Films](https://gsaim.cau.ac.kr/) at Chung-Ang University, where I lead the [Immersive Reality & Integrated Systems (IRIS) Lab](https://www.irislab.cau.ac.kr/).",
+    "I am an Associate Professor in the [Graduate School of Advanced Imaging Science, Multimedia & Film](https://gsaim.cau.ac.kr/) at Chung-Ang University, where I lead the [Immersive Reality & Integrated Systems (IRIS) Lab](https://www.irislab.cau.ac.kr/).",
     "Before joining Chung-Ang University, I worked as a Postdoctoral Researcher in the [Signal Processing Lab (LTS4)](https://www.epfl.ch/labs/lts4/) with Professor [Pascal Frossard](https://www.epfl.ch/labs/lts4/people/people-current/frossard/) and the [Image and Visual Representation Lab (IVRL)](https://www.epfl.ch/labs/ivrl/) with Professor [Sabine Süsstrunk](https://people.epfl.ch/sabine.susstrunk?lang=en) at EPFL, Switzerland. I received my PhD in [Electrical Engineering](https://ee.kaist.ac.kr/) from KAIST under the supervision of Professor [Yong Man Ro](https://www.ivllab.kaist.ac.kr/).",
   ],
   researchInterests: [
